1e56fa20fc237fc586093b4f36fcf8a711679229
mbournehalley <mbournehalley@gmail.com>
2019-06-25 15:34:02 -0700

    feat: adding HTTP service and observables

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { UserSettings } from "../data/user-settings";
 import { NgForm, NgModel } from "@angular/forms";
+import { DataService } from "../data/data.service";
 
 @Component({
   selector: "app-user-settings-form",
@@ -17,7 +18,7 @@ export class UserSettingsFormComponent implements OnInit {
   };
 
   userSettings: UserSettings = { ...this.orignalUserSettings };
-  constructor() {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {}
 
@@ -26,5 +27,11 @@ export class UserSettingsFormComponent implements OnInit {
   }
   onSubmit(form: NgForm) {
     console.log("in submit: ", form.valid);
+    this.dataService
+      .postUserSettingsForm(this.userSettings)
+      .subscribe(
+        result => console.log("success", result),
+        error => console.log("error", error)
+      );
   }
 }
