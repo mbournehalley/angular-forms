94c5841904c9e5bc4387567c41c171a923bd7472
mbournehalley <mbournehalley@gmail.com>
2019-06-25 16:05:26 -0700

    feat: retrieve data for select element

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -9,6 +9,10 @@ import { HttpClient } from "@angular/common/http";
 export class DataService {
   constructor(private http: HttpClient) {}
 
+  getSubscriptionTypes(): any {
+    return of(["Monthly", "Annual", "Lifetime"]);
+  }
+
   postUserSettingsForm(userSettings: UserSettings): Observable<any> {
     return this.http.post(
       "https://putsreq.com/kZPY0wyJYR3o5qGTYXqI",
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { UserSettings } from "../data/user-settings";
 import { NgForm, NgModel } from "@angular/forms";
 import { DataService } from "../data/data.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-user-settings-form",
@@ -19,10 +20,13 @@ export class UserSettingsFormComponent implements OnInit {
   userSettings: UserSettings = { ...this.orignalUserSettings };
   postError = false;
   postErrorMessage = "";
+  subscriptionTypes: Observable<string[]>;
 
   constructor(private dataService: DataService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+  }
 
   onBlur(field: NgModel) {
     console.log("in onBlur: ", field.valid);
